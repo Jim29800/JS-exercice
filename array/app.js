66eab68dd1968f0e8b9266646543c0c204b62eaf
Jim29800 <jbouvier1173@gmail.com>
2017-07-05 11:19:34 +0200

    Exercice 2

--- array/app.js
+++ array/app.js
@@ -1,1 +1,10 @@
 var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
+
+var mois = ["janvier","février","mars","avril","mais","juin","juillet","aout","septembre","octobre","novembre","décembre"]
+
+//exercie 2
+
+console.log(mois[2])
+
+//exercice 3
+
