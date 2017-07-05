9d86f8a4a24f9a3a893e50cf0083fbeefdc18215
Jim29800 <jbouvier1173@gmail.com>
2017-07-05 11:57:06 +0200

    Exercice 10

--- array/app.js
+++ array/app.js
@@ -2,4 +2,18 @@ var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
 
 // exercice 8
 
-array.push("Courgette") 
+array.push("Courgette");
+console.log(array.join());
+
+//exercice 9
+
+array[4].push("Citron");
+console.log(array.join());
+
+// exercice 10
+
+array.splice(1,1);
+console.log(array.join());
+
+// exercice 11
+
