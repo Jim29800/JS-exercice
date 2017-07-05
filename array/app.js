24ad3ba5d4b586494970889299e5df42b7d16358
Jim29800 <jbouvier1173@gmail.com>
2017-07-05 12:22:00 +0200

    Exercice 11

--- array/app.js
+++ array/app.js
@@ -9,11 +9,11 @@ console.log(array.join());
 
 array[4].push("Citron");
 console.log(array.join());
-
 // exercice 10
 
 array.splice(1,1);
 console.log(array.join());
-
 // exercice 11
 
+array[2]="Poire";
+console.log(array.join());
