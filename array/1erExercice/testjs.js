8862e3bb512fefc7590f6b9cae4ecd25cc919a85
Jim29800 <jbouvier1173@gmail.com>
2017-07-05 11:49:00 +0200

    Exercice 8

--- array/1erExercice/testjs.js
+++ array/1erExercice/testjs.js
@@ -3,3 +3,29 @@ var array = ["fraise","citron","banane","tomate"];
 console.log(array[2])
 //recuperer l'index 3 du tableau array
 console.log(array[3])
+
+//creer le tableau
+
+
+var mois = ["janvier","février","mars","avril","mais","juin","juillet","aout","septembre","octobre","novembre","décembre"];
+
+//exercie 2
+
+console.log(mois[2]);
+
+//exercice 3
+
+console.log(mois[5]);
+
+//exercice 4
+
+console.log(mois[10]);
+
+// exercice 5
+
+mois[7]="âout"
+console.log(mois[7]);
+
+// exercice 6
+
+console.log(mois.join());
--- array/app.js
+++ array/app.js
@@ -1,23 +1,5 @@
 var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
 
-var mois = ["janvier","février","mars","avril","mais","juin","juillet","aout","septembre","octobre","novembre","décembre"]
+// exercice 8
 
-//exercie 2
-
-console.log(mois[2])
-
-//exercice 3
-
-console.log(mois[5])
-
-//exercice 4
-
-console.log(mois[10])
-
-// exercice 5
-
-mois[7]="âout"
-console.log(mois[7])
-
-// exercice 6
-
+array.push("Courgette") 
